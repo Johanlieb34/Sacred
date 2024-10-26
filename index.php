9fab4e5ab04ce5204d72e6bc9b4dbbbdaf5c5e38
Johanlieb34 <godswillosamaye810@gmail.com>
2024-10-26 21:21:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!/doctype html>
+<html>
+<head>
+<style>
+body{
+background:linear-gradient(green,yellow);
+    color: white;
+    text-align: center; }
+  h1 {
+  font-size: 45px;
+  }
+  i {
+    font-size:25px;
+    }
+  h2{
+    font-size: 30px;
+    border: 2px solid white;
+    padding: 10%;
+    margin: 12%; 
+    }
+  </style>
+</head>
+<body>
+  <h1>johan Liebert⍢⃝🌹</h1>
+  <h2>
+  Motivation:
+    <br>
+    <i> 
+    I want to learn how to code to be able to hack and create applications. 
+    </i>
+  </h2>
+  <h3>Launching soon....</h3>
+</body>
+<body>
+<title> johan webpage  </title>
+</h4>  hey  welcome to johan  website </h4>
+<button> 🤫🤫🤫 </button>
+<p>
+<a href="https://whatsapp.com/channel/0029VanxGm5J3jv39RvJIM39"> join my private WhatsApp channel </a> <p>
+<a href="https://www.instagram.com/johanlieb34?igshYzljYTk1ODg3Zg"> follow me on Instagram </a> <p>
+<a href="https://www.tiktok.com/@johanlieberttkcodm?_r=1&_d=eemd6ld76ckbaa&sec_uid=MS4wLjABAAAAAJLewSGFOJkj6bJzldwuSyRVfIVUIKc1c_vwsYwEEikKe7Uxr0ZECJMXVga8FpiL&share_author_id=7384827194386072581&sharer_language=en&source=h5_m&u_code=eemd757gcf1b7h&timestamp=1729626742&user_id=7384827194386072581&sec_user_id=MS4wLjABAAAAAJLewSGFOJkj6bJzldwuSyRVfIVUIKc1c_vwsYwEEikKe7Uxr0ZECJMXVga8FpiL&utm_source=copy&utm_campaign=client_share&utm_medium=android&share_iid=7418460961428752133&share_link_id=d498a7ac-cb0d-4fc8-9586-1f62a9f513eb&share_app_id=1233&ugbiz_name=ACCOUNT&ug_btm=b8727%2Cb0229&social_share_type=5&enable_checksum=1"> follow me on tiktok </a> <p>
+</body>

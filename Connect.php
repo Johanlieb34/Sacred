180f3ec9c797df6b17f90f1b3f1f50ef64012570
Johanlieb34 <godswillosamaye810@gmail.com>
2024-10-26 21:04:51 +0100

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,8 +1,1 @@
-<?php
-$con=mysqli_connect("mysql -u sgroot -p -P 3306 -h SG-neat-lupin-5378johan-10644-mysql-master.servers.mongodirector.com --ssl-mode=VERIFY_CA --ssl_ca=<Path to ca.pem file>");//connection 
-if(mysqli_connect_errno())
-{
-	echo "error occured".mysqli_connect_errno();
-}
-
-?>
+jdbc:mysql://SG-neat-lupin-5378johan-10644-mysql-master.servers.mongodirector.com:3306/<johan>?useSSL=true&verifyServerCertificate=true
